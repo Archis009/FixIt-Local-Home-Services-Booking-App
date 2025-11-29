22d45af95e728d6b513c2c4811c5b94d38d42bcc
Archis009 <vedvadnere@gmail.com>
2025-11-29 10:45:44 +0530

    Storing Favorites

--- src/modules/favourites/FavoritesScreen.js
+++ src/modules/favourites/FavoritesScreen.js
@@ -2,3 +2,25 @@ import { useEffect, useState } from "react";
 import { View, Text, StyleSheet, SafeAreaView, FlatList } from "react-native";
 import { providers } from "../../data/providers";
 import ProviderCard from "../providers/ProviderCard";
+
+export default function FavoritesScreen({ navigation }) {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    loadFavorites();
+  }, []);
+
+  const loadFavorites = () => {
+    const favProviders = providers.filter(p => p.fav);
+    setFavorites(favProviders);
+  };
+
+  const handleBook = (provider) => {
+    navigation.navigate("BookingForm", { provider });
+  };
+
+  const toggleFavorite = (providerId) => {
+    // In a real app, this would update the provider's favorite status
+    console.log("Toggle favorite for provider:", providerId);
+  };
+}
